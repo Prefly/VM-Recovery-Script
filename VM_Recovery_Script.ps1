fe29ba2221f94ab80f42877436717ad5c0eb4906
Farhan Ansari <farhan7015@gmail.com>
2018-03-20 14:15:15 +0530

    Update VM_Recovery_Script.ps1

--- VM_Recovery_Script.ps1
+++ VM_Recovery_Script.ps1
@@ -1,30 +1,4 @@
-﻿########################################################################################################################
-# Start of the script - Description, Requirements & Legal Disclaimer
-########################################################################################################################
 # Developed for Rubrik
-################################################
-# Description:
-# This script runs a Recovery Plan for VMs protected by Cloud
-################################################ 
-# Requirements:
-# - Run PowerShell as administrator with command "Set-ExecutionPolcity unrestricted" on the host running the script
-# - A Cloud cluster or EDGE appliance, network access to it and credentials to login
-# - A CSV with the following fields: VMName,Action,DisableNetwork,RemoveNetworkDevices,PowerOn,RunScriptsinLiveMount,PreFailoverScript,PostFailoverScriptDelay,PostFailoverScript,NextVMFailoverDelay,PreFailoverUserPrompt,PostFailoverUserPrompt
-# - Example CSV Line = FileServer1,LiveMount,TRUE,FALSE,TRUE,FALSE,,0,,30,Are you bloody sure?,Has the VM come online?
-# - Valid options for Action are LiveMount, InstantRecover, recommended to use LiveMount first to validate recovery
-# - The options DisableNetwork,PowerOn,RunScriptsinLiveMount are only used LiveMount operations 
-# - Valid options for DisableNetwork,RemoveNetworkDevices,PowerOn,RunScriptsinLiveMount are TRUE or FALSE
-# - Valid options for PostFailoverScriptDelay,NextVMFailoverDelay is 0 - any number of seconds
-# - If no script is specified for PreFailoverScript,PostFailoverScript then nothing is run
-# - If no user prompt is specified for PreFailoverUserPrompt,PostFailoverUserPrompt then the user isn't prompted, this choice can be made per VM
-# - This script always fails over to the latest snapshot available
-################################################
-# Legal Disclaimer:
-# This script is written by Joshua Stenhouse is not supported under any support program or service. 
-# All scripts are provided AS IS without warranty of any kind. 
-# The author further disclaims all implied warranties including, without limitation, any implied warranties of merchantability or of fitness for a particular purpose. 
-# The entire risk arising out of the use or performance of the sample scripts and documentation remains with you. 
-# In no event shall its authors, or anyone else involved in the creation, production, or delivery of the scripts be liable for any damages whatsoever (including, without limitation, damages for loss of business profits, business interruption, loss of business information, or other pecuniary loss) arising out of the use of or inability to use the sample scripts or documentation, even if the author has been advised of the possibility of such damages.
 ################################################
 # Configure the variables below for the Cloud Cluster
 ################################################
